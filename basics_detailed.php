3a1f8dd00da74e9006445686bae4aa6daccbd67f
Макс <maksik290896@yandex.ru>
2026-01-21 07:14:10 +0300

    changes

--- basics_detailed.php
+++ basics_detailed.php
@@ -3,7 +3,23 @@
 $price = 100;
 $discount = 10;
 $total = $price - $discount;
+echo $total;
+echo "<br>";
+echo "<br>";
 
+
+
+echo "Цена: $price<br>";
+echo "Скидка: $discount<br>";
+echo "Итого: $total<br>";
+
+echo "Цена: $price<br>";
+echo "Скидка: $discount<br>";
+echo "Итого: $total<br>";
+
+echo "Цена: $price<br>";
+echo "Скидка: $discount<br>";
+echo "Итого: $total<br>";
 
 
 
